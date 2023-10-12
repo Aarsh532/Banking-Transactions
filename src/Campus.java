4d27ba77e98bea6684b9e38aa6478d66d69db1b1
Aarsh532 <aarshdadan532@gmail.com>
2023-10-12 17:24:00 -0400

    Project 2 Commit

---
+++ src/Campus.java
@@ -0,0 +1,26 @@
+public enum Campus {
+    NEW_BRUNSWICK("0"),
+    NEWARK("1"),
+    CAMDEN("2");
+
+    private final String code;
+
+    private Campus(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public static Campus fromCode(String code) {
+        for (Campus campus : Campus.values()) {
+            if (campus.getCode().equals(code)) {
+                return campus;
+            }
+        }
+        return null;
+
+    }
+
+}
